8d3dd1028e5465d86d72d1d101ed514fdbf6ed86
David Kim <daviddarden11@gmail.com>
2014-02-03 03:15:23 -0800

    revamped site with skeleton

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,1 +1,14 @@
-console.log('This would be the main JS file.');
+---
+---
+console.log('main.js successfully loaded! Now loading other js below.');
+{% include twitter/bootstrap/js/bootstrap.min.js %}
+
+function flip() {
+  document.getElementById('profile').src = "{{BASE_PATH}}/assets/images/profile1.png";
+  // console.log('now entering the neutral zone');
+}
+function flip_back() {
+  document.getElementById('profile').src = "{{BASE_PATH}}/assets/images/profile2.png";
+  // console.log('now leaving the neutral zone');
+}
+

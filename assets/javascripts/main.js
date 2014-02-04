cdb54d85706659651dd5afc4c7b02e324563d610
David Kim <daviddarden11@gmail.com>
2014-02-04 00:07:35 -0800

    404 updated

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,7 +1,6 @@
 ---
 ---
 console.log('main.js successfully loaded! Now loading other js below.');
-// {% include twitter/bootstrap/js/bootstrap.min.js %}
 
 function flip() {
   document.getElementById('profile').src = "{{BASE_PATH}}/assets/images/profile1.png";
